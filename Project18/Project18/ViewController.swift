ae7b47eaa65954ef06529cf56f597cf7fa401148
Bhavin Kapadia <bkapadia@touchbistro.com>
2022-01-08 15:45:23 -0500

    project 18 and rename project 1 directory

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Bhavin Kapadia on 2022-01-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+        
+
+    }
+
+
+}
+
---
+++ Project18_b/Project1/DetailViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Bhavin Kapadia on 2021-05-09.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    
+    var selectedImage: String?
+    var xPicture: Int?
+    var yPicture: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(xPicture!, yPicture!)
+        title = "Picture \(xPicture!) of \(yPicture!)"
+        navigationItem.largeTitleDisplayMode = .never
+        // Do any additional setup after loading the view.
+        assert((selectedImage != nil), "Selected image does not exists")
+
+        if let imageToLoad =  selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
